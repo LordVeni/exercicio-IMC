22f85697922dda0740d371b4df5e933ca5d43d6e
LordVeni <venilsonalcantarasena@gmail.com>
2023-07-31 14:56:13 -0300

    IMC

---
+++ java script - calculo.js
@@ -0,0 +1,27 @@
+let peso = document.getElementById('peso');
+let altura = document.getElementById('altura');
+let resultado = document.getElementById('resultado');
+let total = document.getElementById('total');
+
+resultado.addEventListener('click', function soma() {
+    let peso = Number(document.getElementById('peso').value);
+    let altura = Number(document.getElementById('altura').value);
+    let total = document.getElementById('total');
+
+    total.textContent = peso / (altura*altura);
+})
+
+
+
+
+
+
+
+
+
+
+//function imc(peso, altura) {
+ //   const resultado = peso / (altura * altura);
+ //   return resultado;
+//}
+
